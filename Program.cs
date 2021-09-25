de0cbf392dd39c23ca65dffe978139a5153f0c98
Prajakta-Bondre <radhabondre20@gmail.com>
2021-09-25 10:14:46 +0530

    Convert temperature

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TempConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double celcius = 84;
+            double fahrenheit;
+            Console.WriteLine("Temperature in celsius: " + celcius);
+           
+            fahrenheit = (celcius * 9) / 5 + 32;
+            Console.WriteLine("Fahrenheit: " + fahrenheit);
+
+           
+
+
+        }
+    }
+}
